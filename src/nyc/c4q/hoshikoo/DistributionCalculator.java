b60cfaa1f03bc589d22bf4d2aceb67b6e10a1c72
hoshikoo <hoshikooki@gmail.com>
2015-04-03 01:54:59 -0400

    add webPageSanitizer

--- src/nyc/c4q/hoshikoo/DistributionCalculator.java
+++ src/nyc/c4q/hoshikoo/DistributionCalculator.java
@@ -2,11 +2,25 @@ package nyc.c4q.hoshikoo;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.
 /**
  * Created by Hoshiko on 4/1/15.
  */
 public class DistributionCalculator {
-    //public calculate(File textFile){
-        //return arrayList
-    //}
+    public calculate(File textFile){
+
+        //hashmap (k=chractor, v) for character
+        // if there is not the charactor add.
+        //list of character in hash set
+
+        // it is already in the hashmap add 1 to the value,
+
+        //put hashset to array and sort it.  use .sort
+        //thourgh the array and value on hashMap
+
+        //print the % of each character.
+        public static void main(String[] args) {
+
+        }
+    }
 }
